a2b26639779f746894f0390808df27f92fe26966
James Chee <jamescheeinnovation@gmail.com>
2018-04-29 15:07:15 +0900

    plz work

--- services/MealParser.js
+++ services/MealParser.js
@@ -1,8 +1,8 @@
 class MealParser{
-  const cheerio = require('cheerio');
-  const request = require('request');
-  
-  const init_url = 'https://stu.dje.go.kr/sts_sci_md01_001.do?schulCode=G100000208&schulCrseScCode=4&schulKndScCode=04&schMmealScCode=2';
+  var cheerio = require('cheerio');
+  var request = require('request');
+
+  var init_url = 'https://stu.dje.go.kr/sts_sci_md01_001.do?schulCode=G100000208&schulCrseScCode=4&schulKndScCode=04&schMmealScCode=2';
 
   makeURL()=>{
     var result = this.init_url;
