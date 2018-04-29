5c0c2fb8ec98f77c5aca6084d7c9eed2d4d99801
James Chee <jamescheeinnovation@gmail.com>
2018-04-29 15:03:07 +0900

    bug update

--- services/MealParser.js
+++ services/MealParser.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 class MealParser{
 
-  const init_url = "https://stu.dje.go.kr/sts_sci_md01_001.do?schulCode=G100000208&schulCrseScCode=4&schulKndScCode=04&schMmealScCode=2";
+  var init_url = 'https://stu.dje.go.kr/sts_sci_md01_001.do?schulCode=G100000208&schulCrseScCode=4&schulKndScCode=04&schMmealScCode=2';
 
   makeURL()=>{
     var result = this.init_url;
