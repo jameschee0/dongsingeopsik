bf3615894b3cee19edc8d379c62cfacd24ed97ba
James Chee <jamescheeinnovation@gmail.com>
2018-04-29 15:00:02 +0900

    bug update

--- services/MealParser.js
+++ services/MealParser.js
@@ -1,5 +1,5 @@
 class MealParser{
-  const cheerio = require('cheerio'),
+  const cheerio = require('cheerio');
   const request = require('request');
 
   const init_url = "https://stu.dje.go.kr/sts_sci_md01_001.do?schulCode=G100000208&schulCrseScCode=4&schulKndScCode=04&schMmealScCode=2";
