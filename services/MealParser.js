35256ef9b8bec9be5656a3e85c114f64e2bf2c1a
James Chee <jamescheeinnovation@gmail.com>
2018-05-01 08:46:45 +0900

    please work

--- services/MealParser.js
+++ services/MealParser.js
@@ -18,7 +18,7 @@ function makeURL(){
 }
 
 function doRequest(url){
-  var list = ['급식']; //[8]monday [12]friday
+  var list = ["급식"]; //[8]monday [12]friday
   request(url, function(err, resp, html) {
       if (!err){
         const $ = cheerio.load(html);
