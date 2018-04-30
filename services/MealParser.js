66bf4e4afa6658ff280cb570f5c0fcb60ed6eb2f
James Chee <jamescheeinnovation@gmail.com>
2018-05-01 08:52:46 +0900

    can we approach list?

--- services/MealParser.js
+++ services/MealParser.js
@@ -6,15 +6,15 @@ const init_url = "https://stu.dje.go.kr/sts_sci_md01_001.do?schulCode=G100000208
 const MealParser = {};
 
 MealParser.parse = () => {
-  var result;
+  var result_data;
   console.log(''+init_url);
-  result = doRequest(makeURL());
-  return result;
+  result_data = doRequest(makeURL());
+  return result_data;
 }
 
 function makeURL(){
-  var result = init_url;
-  return result;
+  var result_url = init_url;
+  return result_url;
 }
 
 function doRequest(url){
@@ -24,8 +24,8 @@ function doRequest(url){
         const $ = cheerio.load(html);
         $('.tbl_type3 th:contains("중식")').parent().children().each(function () {
           //list.push($(this).text());
-          //var content = $(this).text();
-          //console.log(content+"///////");
+          var content = $(this).text();
+          console.log(content+"///////");
         });
         return list[0];
     }else{
