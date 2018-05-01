0d59bb0f3c68031d9384e6776dd92bf111900304
James Chee <jamescheeinnovation@gmail.com>
2018-05-01 14:24:38 +0900

    working?

--- services/MealParser.js
+++ services/MealParser.js
@@ -4,28 +4,31 @@ const cheerio = require("cheerio"),
 const init_url = "https://stu.dje.go.kr/sts_sci_md01_001.do?schulCode=G100000208&schulCrseScCode=4&schulKndScCode=04&schMmealScCode=2";
 
 const MealParser = {};
-var list = ["initial array"];
-var ongoing = true;
+
 MealParser.parse = () => {
   console.log(''+init_url);
-  request(makeURL(), function(err, resp, html) {
-      if (!err){
-      const $ = cheerio.load(html);
-      $('.tbl_type3 th:contains("중식")').parent().children().each(function () {
-        list.push($(this).text());
-      });
-      console.log(list);
-      ongoing = false;
-    }
+  return doRequest(makeURL(),function(err,data){
+    console.log(data);
+    return data;
   });
-  while(ongoing){
-  }
-  return list[4];
 }
 
 function makeURL(){
   var result_url = init_url;
   return result_url;
+}
+
+function doRequest(url,callback){
+  request(url, function(err, resp, html) {
+      var list = ["initial array"];
+      if (!err){
+      const $ = cheerio.load(html);
+      $('.tbl_type3 th:contains("중식")').parent().children().each(function () {
+        list.push($(this).text());
+      });
+      return callback(null,list);
+    }
+  });
 }
 
 module.exports = MealParser;
