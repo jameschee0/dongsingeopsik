f7bb3fb91a9c5d999acba3755298931ee2413a1b
James Chee <jamescheeinnovation@gmail.com>
2018-05-01 16:52:46 +0900

    this will work

--- app.js
+++ app.js
@@ -90,49 +90,12 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
-    var result = MealParser.parse();
     if(received_message.text==='오늘의 급식'){
-      response={
-        "text":result,
-        "quick_replies":[
-          {
-          "content_type":"text",
-          "title":"오늘의 급식",
-          "payload":"GET_MENU_PAYLOAD",
-          }
-        ]
-      }
+      MealParser.sendMeal();
     }
-  } else if (received_message.attachments) {
-    // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
-            ],
-          }]
-        }
-      }
-    }
+  }else{
+    MealParser.sendMeal();
   }
-
   // Send the response message
   callSendAPI(sender_psid, response);
 }
--- services/MealParser.js
+++ services/MealParser.js
@@ -2,14 +2,38 @@ const cheerio = require("cheerio"),
       request = require("request");
 
 const init_url = "https://stu.dje.go.kr/sts_sci_md01_001.do?schulCode=G100000208&schulCrseScCode=4&schulKndScCode=04&schMmealScCode=2";
+const response_message = {
+  "text":"ey!!",
+  "quick_replies":[
+      {
+      "content_type":"text",
+      "title":"오늘의 급식",
+      "payload":"GET_MENU_PAYLOAD",
+      }
+    ]
+  }
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 const MealParser = {};
 
-MealParser.parse = () => {
+MealParser.sendMeal = () =>{
   console.log(''+init_url);
-  return doRequest(makeURL(),function(err,data){
+  parse(makeURL(),function(err,data){
     console.log(data);
-    return data;
+    callSendAPI(sender_psid, makeResponse(data));
+  });
+}
+
+function parse(url,callback){
+  request(url, function(err, resp, html) {
+      var list = ["initial array"];
+      if (!err){
+      const $ = cheerio.load(html);
+      $('.tbl_type3 th:contains("중식")').parent().children().each(function () {
+        list.push($(this).text());
+      });
+      callback(null,list[4]);
+    }
   });
 }
 
@@ -18,15 +42,31 @@ function makeURL(){
   return result_url;
 }
 
-function doRequest(url,callback){
-  request(url, function(err, resp, html) {
-      var list = ["initial array"];
-      if (!err){
-      const $ = cheerio.load(html);
-      $('.tbl_type3 th:contains("중식")').parent().children().each(function () {
-        list.push($(this).text());
-      });
-      return callback(null,list);
+function makeResponse(data){
+  response_message.text = data;
+  return response_message;
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
     }
   });
 }
