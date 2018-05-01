623c5704c616135f306dd2f8d9cec36c2ca00157
James Chee <jamescheeinnovation@gmail.com>
2018-05-01 14:20:09 +0900

    added ongoing

--- services/MealParser.js
+++ services/MealParser.js
@@ -4,32 +4,28 @@ const cheerio = require("cheerio"),
 const init_url = "https://stu.dje.go.kr/sts_sci_md01_001.do?schulCode=G100000208&schulCrseScCode=4&schulKndScCode=04&schMmealScCode=2";
 
 const MealParser = {};
-var result_data="sorry error";
-
+var list = ["initial array"];
+var ongoing = true;
 MealParser.parse = () => {
   console.log(''+init_url);
-  return doRequest(makeURL(),function(err,data){
-    console.log(data);
-    return "안녕";
-  });
-}
-
-function makeURL(){
-  var result_url = init_url;
-  return result_url;
-}
-
-function doRequest(url,callback){
   request(url, function(err, resp, html) {
-      var list = ["안녕","세상"];
       if (!err){
       const $ = cheerio.load(html);
       $('.tbl_type3 th:contains("중식")').parent().children().each(function () {
         list.push($(this).text());
       });
-      return callback(null,list);
+      console.log(list);
+      ongoing = false;
     }
   });
+  while(ongoing){
+  }
+  return list[4];
+}
+
+function makeURL(){
+  var result_url = init_url;
+  return result_url;
 }
 
 module.exports = MealParser;
