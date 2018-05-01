eb82d9425c132b4356f30baa00df57ac9373817a
James Chee <jamescheeinnovation@gmail.com>
2018-05-01 13:02:11 +0900

    added changes

--- services/MealParser.js
+++ services/MealParser.js
@@ -5,14 +5,12 @@ const init_url = "https://stu.dje.go.kr/sts_sci_md01_001.do?schulCode=G100000208
 
 const MealParser = {};
 var result_data="sorry error";
-var ongoing = true;
 
 MealParser.parse = () => {
   console.log(''+init_url);
   result_data = doRequest(makeURL(),function(err,data){
     console.log(data);
-    ongoing = false;
-    yield data[4];
+    return data[4];
   });
   return result_data;
 }
